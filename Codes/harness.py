a4925bb0f0a71ba4569e4a58e688bd3a3ce6e4a8
Andrew Deur <andrewrdeur@gmail.com>
2023-11-13 19:59:40 -0500

    changed prediction function to harness

--- Codes/harness.py
+++ Codes/harness.py
@@ -5,13 +5,13 @@ from preprocessor import preprocessing_func, default_check, consolidate_ateco_co
 from estimate import estimation
 from prediction import predict_function
 from walk_forward import bootstrapped_walk_forward_harness
-from utils import predict_harness
+from prediction import predict_harness
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_csv", type=str, help="enter some quality limit",
-                    nargs='?', default='train.csv', const=0)
+                    nargs='?', default='csv_files/train.csv', const=0)
 parser.add_argument("--output_csv", type=str, help="enter some quality limit",
-                    nargs='?', default='train.csv', const=0)
+                    nargs='?', default='csv_files/pds_out.csv', const=0)
 args = parser.parse_args()
 
 input_file = args.input_csv
@@ -24,12 +24,12 @@ model_type='Logit'
 
 preproc_params = {
     "statement_offset" : 6,
-    "ir_path": "ECB Data Portal_20231029154614.csv",
+    "ir_path": "csv_files/ECB Data Portal_20231029154614.csv",
     "features": ['asset_turnover', 'leverage_ratio', 'roa','interest_rate', 'AR', 'stmt_date', 'id']
 }
 
 test = preprocessing_func(test, preproc_params, label=False, interest_rates=True)
-predictions = predict_function(test_df = test, model= model, model_type = model_type)
+predictions = predict_harness(test, model, model_type, plot_auc=False)
 
 pd.DataFrame({
             "PD":list(predictions)
