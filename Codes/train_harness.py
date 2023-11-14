57b31562f6dde971704a44d92ce24723afdf7724
Andrew Deur <andrewrdeur@gmail.com>
2023-11-13 23:20:12 -0500

    changed to random forest

--- Codes/train_harness.py
+++ Codes/train_harness.py
@@ -6,7 +6,7 @@ import numpy as np
 import statsmodels.api as sm
 from sklearn.ensemble import RandomForestClassifier
 import joblib
-from preprocessor import preprocessing_func, default_check, consolidate_ateco_codes, merge_interest_rates, label_defaults, financial_ratios, categorical_to_csv
+from preprocessor import preprocessing_func
 from estimate import estimation
 from prediction import predict_function
 from walk_forward import bootstrapped_walk_forward_harness
@@ -34,15 +34,15 @@ print(df_processed.columns)
 '''
 preproc_params = {
     "statement_offset" : 6,
-    "ir_path": r"/Users/chitvangoyal/Desktop/3001_project/3001-ML-Finance-Project/ECB Data Portal_20231029154614.csv",
-    "features": ['asset_turnover', 'leverage_ratio', 'roa','interest_rate', 'AR', 'stmt_date', 'id']
+    "ir_path": "csv_files/ECB Data Portal_20231029154614.csv",
+    "features": ['asset_turnover', 'leverage_ratio', 'roa','interest_rate', 'AR', 'ateco_industry', 'legal_struct','stmt_date', 'id']
 }
 
-df = pd.read_csv('/Users/chitvangoyal/Desktop/3001_project/3001-ML-Finance-Project/train.csv')
+df = pd.read_csv('csv_files/train.csv')
 df.head()
 start_index = df['stmt_date'].min()
-model_type = 'Logit'
+model_type = 'Random_Forest'
 model, test_stats_list, out_of_sample_stats_list = bootstrapped_walk_forward_harness(df, preprocessor_function = preprocessing_func, preproc_params=preproc_params, train_function = estimation, start_index=start_index, step_size=1, num_bootstrap_samples=10, model_type=model_type)
 
-filename = 'basic_model.sav'
+filename = 'rf_model.sav'
 joblib.dump(model, filename)
